4c1e464d76c81fae5da8c32a1743e44801e8c51c
Zibing Zhang <zibing.zha@gmail.com>
2026-08-14 21:21:12 -0400

    Fix birthday command

--- contacts/command/birthday.py
+++ contacts/command/birthday.py
@@ -1,4 +1,5 @@
 """Command to push contacts to a remote source."""
+
 from __future__ import annotations
 
 import datetime
@@ -16,15 +17,23 @@ def run(*, days: int) -> None:
     max_day = min_day + datetime.timedelta(days=days)
     min_day_int = 100 * min_day.month + min_day.day
     max_day_int = 100 * max_day.month + max_day.day
-
     same_year = min_day.year == max_day.year
 
+    contacts = []
     for contact in disk_contacts:
         birthday = 100 * contact.birthday.month + contact.birthday.day
-
         if same_year:
             if min_day_int <= birthday <= max_day_int:
-                LOG.info(contact.name, contact.birthday)
+                contacts.append(contact)
         else:
             if birthday >= min_day_int or birthday <= max_day_int:
-                LOG.info(contact.name, contact.birthday)
+                contacts.append(contact)
+
+    max_name_length = max(
+        1 + len(contact.name.first_name) + len(contact.name.last_name)
+        for contact in contacts
+    )
+    for contact in contacts:
+        LOG.info(
+            f"{f'{contact.name.first_name} {contact.name.last_name}'.ljust(max_name_length)} -- {contact.birthday}"
+        )
